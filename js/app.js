a49b858167eaa52e27f8ec8d94e7fe351372619c
ShineYH <390802826@qq.com>
2017-02-19 18:58:54 +0800

    Vue任务便签 ：数据绑定 点击添加，不操作界面dom思想

--- js/app.js
+++ js/app.js
@@ -3,13 +3,29 @@
  */
 
 var list = [
-    {title:"吃饭"},
-    {title:"睡觉"}
+    /*{title:"吃饭"},
+    {title:"睡觉"}*/
 ];
 
 new Vue({
-    el:".mian",
+    el:".main",
     data:{
-        list:list
+        list:list,
+        todo:""
+    },
+    methods:{
+        addTodo:function(ev){
+           // if(ev.keyCode ===13){               <!-- keyup.enter  事件修饰符，判断enter键 这里即可不做判断-->
+               /* this.list.push({                   //this 指向的都是Vue的根实例
+                    title:ev.target.value
+                });*/
+          //  }
+            // 上面方法还是操纵了dom元素，以下设计一个方法，不用操纵dom，data里面自定义一个属性，与input值绑定，然后取这个里面的值
+
+            this.list.push({
+                title:this.todo
+             });
+            this.todo = "";   //渲染之后 还要清空
+        }
     }
 });
