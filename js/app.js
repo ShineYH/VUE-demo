9ba5e8c08161e997d9f73e28a88f487c6fddce39
ShineYH <390802826@qq.com>
2017-02-19 19:04:54 +0800

    Vue任务便签 如果需要传参数的同时 传事件处理函数，需手动传一个$event的对象

--- js/app.js
+++ js/app.js
@@ -14,14 +14,14 @@ new Vue({
         todo:""
     },
     methods:{
-        addTodo:function(ev){
+        addTodo:function(number,ev){       //如何既传参数，也将事件处理函数传过来  html页面 传一个$event对象
+           // console.info(ev);
            // if(ev.keyCode ===13){               <!-- keyup.enter  事件修饰符，判断enter键 这里即可不做判断-->
                /* this.list.push({                   //this 指向的都是Vue的根实例
                     title:ev.target.value
                 });*/
           //  }
             // 上面方法还是操纵了dom元素，以下设计一个方法，不用操纵dom，data里面自定义一个属性，与input值绑定，然后取这个里面的值
-
             this.list.push({
                 title:this.todo
              });
